f04c6dcc214cdf4bf37c28aadca28c71a208d33d
Blakely <bn.frechette@gmail.com>
2020-01-23 10:59:35 -0800

    Separated Sidebar from Dashboard

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
 import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
-import List from '@material-ui/core/List';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import MenuIcon from '@material-ui/icons/Menu';
 
-import { mainListItems, secondaryListItems } from './components/ListItems';
 import Sched from '../scheduler/Sched';
 import DropDown from './components/DropDown';
 import Topbar from './components/Topbar';
+import Sidebar from './components/Sidebar';
 
 const drawerWidth = 240;
 
@@ -112,43 +103,9 @@ const Dashboard = props => {
     })}>
       <CssBaseline /> 
 
-      {/* Header */}
-      {/* <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            ME Department Machine Shop
-          </Typography>
-        </Toolbar>
-      </AppBar> */}
       <Topbar onDrawerOpen={handleDrawerOpen} isOpen={open} />
 
-      {/* SideNav */}
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>{mainListItems}</List>
-        <Divider />
-        <List>{secondaryListItems}</List>
-      </Drawer>
+      <Sidebar onDrawerClose={handleDrawerClose} isOpen={open} />
 
       {/* Main Content */}
       <main className={classes.content}>
