13459e2132523fda70b1768afb5b8040ebfc0a4e
Blakely <bn.frechette@gmail.com>
2020-01-23 11:12:16 -0800

    Reorganized Code Structure

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
-import Dashboard from './components/dashboard/Dashboard';
-import SignInSide from './components/signin/SignInSide';
-import Reservation from './components/reservation/Reservation';
+import Dashboard from './layouts/dashboard/Dashboard';
+import SignInSide from './views/signin/SignInSide';
+import Reservation from './views/reservation/Reservation';
 
 const Routes = () => {
     return (
--- client/src/layouts/dashboard/Dashboard.js
+++ client/src/layouts/dashboard/Dashboard.js
@@ -7,70 +7,14 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Sched from '../scheduler/Sched';
+import Sched from '../../components/scheduler/Sched';
 import DropDown from './components/DropDown';
 import Topbar from './components/Topbar';
 import Sidebar from './components/Sidebar';
 
-const drawerWidth = 240;
-
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
